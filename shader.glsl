84b44bc116ac5e85683564f57b5928aae53b47fd
tony.dorfmeister <tony.dorfmeister@bepic.studio>
2021-01-27 23:18:53 +0100

    sphere yay

--- shader.glsl
+++ shader.glsl
@@ -1,7 +1,8 @@
 #define MAX_RAY_STEP 100
 #define MAX_DISTANCE 100.
-#define MIN_HIT_DIST .001
+#define MIN_HIT_DIST .01
 
+uniform vec3 uray_origin;
 
 // Example Pixel Shader
 
@@ -11,7 +12,7 @@ out vec4 fragColor;
 
 
 float get_distance(vec3 pos){
-	vec4 sphere_origin = vec4(0.,1.,5.,1.);
+	vec4 sphere_origin = vec4(0.,1.,5.,1);
 
 	float distance_sphere = length(pos-sphere_origin.xyz) - sphere_origin.w;
 	float distance_ground = pos.y;
@@ -42,35 +43,28 @@ float ray_march(vec3 ray_origin, vec3 ray_direction){
 ////////////////////////////////
 vec4 ray_main(vec2 uv){
 	
-	vec3 ray_origin = vec3(0., 1., 0.);
-	vec3 ray_direction = vec3(uv, 1.);	//nice 
+	vec3 color = vec3(0.);
+
+	// vec3 ray_origin = vec3(-5., 1., 0.);
+	vec3 ray_origin = uray_origin;
+	vec3 ray_direction = vec3(uv.x, uv.y, 1.);	//nice 
 	
 	float distance = ray_march(ray_origin, ray_direction);
 	
+	color = vec3(distance)/5;
 	
-	
-	return vec4(1.);
+	return vec4(color, 1.);
 }
 
 
 
-
-
-
-
-
-
-
-////////////////////////////////
-////////////////////////////////
-////////////////////////////////
 void main()
 {
-	vec2 uv = vUV.st * 2. - 1.;
+	vec2 uv = vUV.st * 2 - 0.25 ;
+	// vec2 uv = vUV.st - 0.5 * vec2(1024, 1024) ;
 	
 	
-	
-	vec4 color = ray_main(vUV.st);
+	vec4 color = ray_main(uv);
 	fragColor = TDOutputSwizzle(color);
 
 }
