a4f8c08d160f18ca367a49accad2d7ded9ba76b2
tony.dorfmeister <tony.dorfmeister@bepic.studio>
2021-02-11 23:14:41 +0100

    voronoii voll geil yoooo

---
+++ voronoi.glsl
@@ -0,0 +1,49 @@
+
+uniform int cell_number;
+uniform float uTime;
+uniform vec2 umouse_pos;
+
+
+out vec4 fragColor;
+
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+void main()
+{
+    vec3 color = vec3(0.);
+	vec2 uv = vUV.st;
+    vec2 point[12];
+	uv = uv ;//- 0.5;	//DONT MOVE DA UV'S
+
+    float m_dist = 1;
+    vec2 m_point;
+
+    for(int i = 0; i < cell_number; i++)
+    {
+        if (mod(2,i)){
+            point[i] = vec2(rand(vec2(uTime,i)),rand(vec2(10*uTime,i))) + sin(umouse_pos);
+        }else{
+            point[i] = vec2(rand(vec2(uTime,i)),rand(vec2(10*uTime,i)));
+        }
+
+        //point[i] *= 0.5 + 0.5*sin(vec2(uTime)*point[i]);
+
+        float dist = distance(uv, point[i]);
+        if( dist < m_dist){
+            m_dist = dist;
+
+            m_point = point[i];
+        }
+    }
+
+    color += m_dist*2;
+
+    color.rg = m_point;
+
+
+	fragColor = TDOutputSwizzle(vec4(color, 1));
+}
+
+
